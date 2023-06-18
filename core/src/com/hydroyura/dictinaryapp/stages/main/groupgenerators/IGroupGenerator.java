2838c9023c7c453f0df15e4c8798ddb313492858
yury_klimchuk <hydro.yura@gmail.com>
2023-06-18 21:38:18 +0300

    init - 18.06.23::0

--- core/src/com/hydroyura/dictinaryapp/stages/main/groupgenerators/IGroupGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.hydroyura.dictinaryapp.stages.main.groupgenerators;
-
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-
-public interface IGroupGenerator {
-    Group generate(Skin skin);
-}
